131534c0ab959ef18252c4527d2c3321adb28ca2
graceldt <grace.lendet@gmail.com>
2024-10-15 12:31:35 +0200

    ajout image projet

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -3,6 +3,8 @@ import Booki from "../images/booki.webp"
 import Nina from "../images/nina_carducci.webp"
 import Ohmyfood from "../images/ohmyfood.webp"
 import Sophie from "../images/sophie_bluel.webp"
+import Kasa from "../images/kasa.webp"
+import ArgentBank from "../images/argentBank.webp"
 
 
 function Projects (){
@@ -13,9 +15,9 @@ function Projects (){
             <Card title="Booki" image={Booki} tags={['HTML', 'CSS']} description="Création de la page d'accueil d'une agence de voyage"/>
             <Card title="OhmyFood" image={Ohmyfood} tags={['HTML', 'SCSS']}  description="Amélioration de l'interface d'un site mobile avec des animations"/>
             <Card title="Sophie Bluel" image={Sophie} tags={['HTML','CSS', 'JAVASCRIPT']} description="Création d'une page web dynamique avec JavaScript"/>
-            <Card title="Kasa" image={Booki} tags={['HTML','CSS', 'REACT']} description="Création d'une application web de location immobilière avec React"/>
+            <Card title="Kasa" image={Kasa} tags={['HTML','CSS', 'REACT']} description="Création d'une application web de location immobilière avec React"/>
             <Card title="Nina Carducci" image={Nina} tags={['HTML','CSS', 'JAVASCRIPT']} description="Optimisation du référencement d'un site de photographe"/>
-            <Card title="Argent Bank" image={Booki} tags={['HTML','CSS', 'REACT']} description="Implémentation du front-end d'une application bancaire avec React"/>
+            <Card title="Argent Bank" image={ArgentBank} tags={['HTML','CSS', 'REACT']} description="Implémentation du front-end d'une application bancaire avec React"/>
 
             </div>
         </div>
